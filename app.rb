8a31cf1bc59d77db0a7750d5b43b1c651bd3a530
chaseWilliams <bornofawesomeness@gmail.com>
2016-02-29 21:53:00 -0500

    added error handling; redis now properly refreshes articles rather than storing once and using the same article over and over again

--- app.rb
+++ app.rb
@@ -5,20 +5,25 @@ class App < Sinatra::Base
     content_type 'application/json'
   end
 
-  get '/standard' do
-    data = Article.find_article params[:latitude].round(3), params[:longitude].round(3)
-    {status: 'ok', title: data[:title], extract: data[:extract]}.to_json
-  end
-
-  error
-  get '/zip_code' do
+  get '/' do
     wikipedia = Article::Proxy.new
-    zip_code = params[:zip_code]
-    if zip_code.class == Fixnum then zip_code.to_s end
-    latitude = zip_code.to_lat.to_f.round(3)
-    longitude = zip_code.to_lon.to_f.round(3)
-    data = wikipedia.find_article latitude, longitude
-    Logger.new(STDOUT).info "Data successfully transported! #{data}. \nClass: #{data.class}"
-    {status: 'ok', title: data['title'], extract: data['extract']}.to_json
+    begin
+      if params.has_key?('lat') && params.has_key?('lon')
+        latitude = params[:lat].to_f
+        longitude = params[:lon].to_f
+      else
+        zipcode = params[:zipcode].to_s
+        latitude = zipcode.to_lat.to_f.round(3)
+        longitude = zipcode.to_lon.to_f.round(3)
+      end
+    rescue StandardError => e
+      status 400
+      Logger.new(STDOUT).warn '400 raised'
+      'Looks like you gave a bad request- please try again after changing your URL'
+    else
+      data = wikipedia.find_article latitude, longitude
+      Logger.new(STDOUT).info "Data successfully Obtained! #{data}."
+      {status: data['status'], title: data['title'], extract: data['extract']}.to_json
+    end
   end
 end
--- wikipedia/wikipedia.rb
+++ wikipedia/wikipedia.rb
@@ -4,36 +4,47 @@ module Article
     def initialize
       @redis = Redis.new(url: ENV['REDIS_URI'])
       @logger_out = Logger.new(STDOUT)
-      @logger_err = Logger.new(STDERR)
     end
-    def find_article lat, long
-      geo = "#{lat}|#{long}"
-      unless @redis.exists geo
-        @logger_out.info "The key #{geo} wasn't found, so fetching new article extracts"
-        data = find_article_helper lat, long
-        @redis.set geo, data.to_json
-        @logger_out.info "The key #{geo} was successfully set with this information: \nTitle: #{data[:title]}\n\n
-#{data[:extract]}\n\n"
+    def find_article lat, lon
+      geo = "#{lat}|#{lon}"
+      data = find_article_helper lat, lon
+      if !@redis.exists(geo)
+        @logger_out.info "The key #{geo} wasn't found, so setting new article extracts"
+        unless data['status'] == 'ok'
+          @logger_out.warn "Woah- looks like the key #{geo} has a fail status. Watch out!"
+        else
+          @logger_out.info "The key #{geo} was successfully set with this information: \nTitle: #{data[:title]}\n\n
+                           #{data[:extract]}\n\n"
+        end
+      else
+        @logger_out.info "The key #{geo} was found, overriding..."
       end
+      @redis.set geo, data.to_json
       result = JSON.parse @redis.get geo
       @logger_out.info "Key #{geo} successfully captured!"
       result
     end
-    def find_article_helper lat, long
+
+    def find_article_helper lat, lon
       endpoint_base = 'https://en.wikipedia.org/w/api.php?action=query&'
-      endpoint_geo = 'format=json&list=geosearch&gsradius=10000&gscoord='
+      endpoint_geo = 'format=json&list=geosearch&gsradius=10000&gslimit=100&gscoord='
       endpoint_article = 'prop=extracts&format=json&titles='
-      response = JSON.parse RestClient::Request.execute(:url => URI.encode(
-          endpoint_base + endpoint_geo + "#{lat}|#{long}"), :method => :get, :verify_ssl => false)
-      size = response['query']['geosearch'].size
-      title = response['query']['geosearch'][rand size]['title']
-      response = JSON.parse RestClient::Request.execute(:url => URI.encode(
-          endpoint_base + endpoint_article + "#{title}"), :method => :get, :verify_ssl => false)
-      hash_keys = response['query']['pages'].keys
-      result = response['query']['pages'][hash_keys[0]]['extract']
-      index = result.index '</p>'
-      extract = result.slice 0, index
-      {title: title, extract: extract}
+      begin
+        response = JSON.parse RestClient::Request.execute(:url => URI.encode(
+            endpoint_base + endpoint_geo + "#{lat}|#{lon}"), :method => :get, :verify_ssl => false)
+        size = response['query']['geosearch'].size
+        title = response['query']['geosearch'][rand size]['title']
+        response = JSON.parse RestClient::Request.execute(:url => URI.encode(
+            endpoint_base + endpoint_article + "#{title}"), :method => :get, :verify_ssl => false)
+        hash_keys = response['query']['pages'].keys
+        result = response['query']['pages'][hash_keys[0]]['extract']
+        index = result.index '</p>'
+        extract = result.slice 0, index
+        to_be_returned = {status: 'ok', title: title, extract: extract.strip}
+      rescue StandardError => e
+        to_be_returned = {status: 'fail'}
+      end
+      to_be_returned
     end
   end
 end
